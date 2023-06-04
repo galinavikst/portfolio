ea3a8cb4d758be0b1c48543d4596bc34756bea06
Halyna Stepanenko <galinavikst@gmail.com>
2023-06-04 18:47:53 +0200

    init: first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>hello</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
