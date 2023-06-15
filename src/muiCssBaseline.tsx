8af72a3997cff6ead629d94199eddad9b986580e
Halyna Stepanenko <galinavikst@gmail.com>
2023-06-15 22:19:44 +0300

    refactor: delete unuseble code

--- src/muiCssBaseline.tsx
+++ src/muiCssBaseline.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import { createTheme } from "@mui/material/styles";
-import createTypography from "@mui/material/styles/createTypography";
-import createPalette from "@mui/material/styles/createPalette";
 
 export const theme = createTheme({
   components: {
