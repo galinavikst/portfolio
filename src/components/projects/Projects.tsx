8f2281e223fe7d65383e302fe365dd9f1c6e54ce
Halyna Stepanenko <galinavikst@gmail.com>
2023-06-19 19:19:41 +0300

    fix: test deploy

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -5,6 +5,10 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import carPath from "../../assets/car.mp4";
+import votePath from "../../assets/vote.mp4";
+import weatherPath from "../../assets/weather.mp4";
+import zooPath from "../../assets/zoo.mp4";
 
 interface IPaths {
   path: string;
@@ -33,28 +37,28 @@ export default function Projects() {
 function MediaCardList() {
   const paths: IPaths[] = [
     {
-      path: "../../public/vote.mp4",
+      path: votePath,
       title: "Vote App",
       description:
         "Allows users to create questions with multiple answer options and conduct voting on them. The results of the voting are dynamically calculated and displayed in the form of voting count and percentage. The app also includes form validation and uses Redux Toolkit and React Router for navigation.",
       technology: "React, Redux, TS, CSS, Animate.css, Responsive",
     },
     {
-      path: "../../public/car.mp4",
+      path: carPath,
       title: "Tire Fitting",
       description:
         "User-friendly interface that allows drivers to easily submit appointment requests for tire fitting. Form validation that ensures that all required fields are filled out correctly before the appointment request is submitted.",
       technology: "HTML, CSS, JS, jQuery, Responsive",
     },
     {
-      path: "../../public/weather.mp4",
+      path: weatherPath,
       title: "Weather App",
       description:
         "Displays weather data for the current moment, today, and the next five days. Allows users to search for any city or use their current geolocation.",
       technology: "JS, API, Bootstrap, Axios, Responsive",
     },
     {
-      path: "../../public/zoo.mp4",
+      path: zooPath,
       title: "Online zoo",
       description:
         "The home page provides an overview of the app and the animals available for viewing. The donate page allows users to make a donation to their chosen animal.",
