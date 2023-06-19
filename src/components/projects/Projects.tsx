5ad5243198df709d88a60bbd942f3d767d78003d
Halyna Stepanenko <galinavikst@gmail.com>
2023-06-19 17:00:57 +0300

    fix: move video to public folder

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -33,28 +33,28 @@ export default function Projects() {
 function MediaCardList() {
   const paths: IPaths[] = [
     {
-      path: "../../src/assets/vote.mp4",
+      path: "../../public/vote.mp4",
       title: "Vote App",
       description:
         "Allows users to create questions with multiple answer options and conduct voting on them. The results of the voting are dynamically calculated and displayed in the form of voting count and percentage. The app also includes form validation and uses Redux Toolkit and React Router for navigation.",
       technology: "React, Redux, TS, CSS, Animate.css, Responsive",
     },
     {
-      path: "../../src/assets/car.mp4",
+      path: "../../public/car.mp4",
       title: "Tire Fitting",
       description:
         "User-friendly interface that allows drivers to easily submit appointment requests for tire fitting. Form validation that ensures that all required fields are filled out correctly before the appointment request is submitted.",
       technology: "HTML, CSS, JS, jQuery, Responsive",
     },
     {
-      path: "../../src/assets/weather.mp4",
+      path: "../../public/weather.mp4",
       title: "Weather App",
       description:
         "Displays weather data for the current moment, today, and the next five days. Allows users to search for any city or use their current geolocation.",
       technology: "JS, API, Bootstrap, Axios, Responsive",
     },
     {
-      path: "../../src/assets/zoo.mp4",
+      path: "../../public/zoo.mp4",
       title: "Online zoo",
       description:
         "The home page provides an overview of the app and the animals available for viewing. The donate page allows users to make a donation to their chosen animal.",
