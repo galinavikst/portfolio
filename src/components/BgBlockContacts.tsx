c6ac5bb24c49c5bfeecef493c573345f960d320e
Halyna Stepanenko <galinavikst@gmail.com>
2023-07-07 23:19:00 +0300

    test bg as img

--- src/components/BgBlockContacts.tsx
+++ src/components/BgBlockContacts.tsx
@@ -6,12 +6,13 @@ function BgBlockContacts() {
       sx={{ p: 5, position: "relative", height: 200, overflow: "auto" }}
     >
       <Box
+        component="img"
         sx={{
-          backgroundImage: "url('./src/assets/back-img.webp')",
-          WebkitBackgroundSize: "cover",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundAttachment: "fixed",
+          //backgroundImage: "url('./src/assets/back-img.webp')",
+          //WebkitBackgroundSize: "cover",
+          // backgroundSize: "cover",
+          // backgroundRepeat: "no-repeat",
+          // backgroundAttachment: "fixed",
           position: "fixed",
           height: "100%",
           width: "100%",
@@ -19,7 +20,21 @@ function BgBlockContacts() {
           top: 0,
           left: 0,
         }}
-      ></Box>
+        alt="universe"
+        src="./src/assets/back-img.jpg"
+      >
+        {/* <Box
+          component="img"
+          sx={{
+            height: 233,
+            width: 350,
+            maxHeight: { xs: 233, md: 167 },
+            maxWidth: { xs: 350, md: 250 },
+          }}
+          alt="universe"
+          src="./src/assets/back-img.webp"
+        /> */}
+      </Box>
       <Typography variant="h1" color={"black"}>
         hi my name is Halyna I'm trying to create cool block hi my name is
         Halyna I'm trying to create cool block hi my name is Halyna I'm trying
