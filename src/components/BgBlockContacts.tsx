a41a7ab0d82b557ff7e7330b1279c1067a7bf811
Halyna Stepanenko <galinavikst@gmail.com>
2023-07-07 23:40:57 +0300

    test z-index

--- src/components/BgBlockContacts.tsx
+++ src/components/BgBlockContacts.tsx
@@ -6,13 +6,23 @@ function BgBlockContacts() {
       sx={{ p: 5, position: "relative", height: 200, overflow: "auto" }}
     >
       <Box
+        sx={{
+          backgroundImage: "url('./src/assets/back-img.webp')",
+          WebkitBackgroundSize: "cover",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+          position: "fixed",
+          height: "100%",
+          width: "100%",
+          //zIndex: -1,
+          top: 0,
+          left: 0,
+        }}
+      />
+      {/* <Box
         component="img"
         sx={{
-          //backgroundImage: "url('./src/assets/back-img.webp')",
-          //WebkitBackgroundSize: "cover",
-          // backgroundSize: "cover",
-          // backgroundRepeat: "no-repeat",
-          // backgroundAttachment: "fixed",
           position: "fixed",
           height: "100%",
           width: "100%",
@@ -22,19 +32,7 @@ function BgBlockContacts() {
         }}
         alt="universe"
         src="./src/assets/back-img.webp"
-      >
-        {/* <Box
-          component="img"
-          sx={{
-            height: 233,
-            width: 350,
-            maxHeight: { xs: 233, md: 167 },
-            maxWidth: { xs: 350, md: 250 },
-          }}
-          alt="universe"
-          src="./src/assets/back-img.webp"
-        /> */}
-      </Box>
+      /> */}
       <Typography variant="h1" color={"black"}>
         hi my name is Halyna I'm trying to create cool block hi my name is
         Halyna I'm trying to create cool block hi my name is Halyna I'm trying
