bb17485ade5e16c18e09b978c1d1436acb1ce693
Halyna Stepanenko <galinavikst@gmail.com>
2023-07-16 02:33:39 +0300

    feat: add gsap

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,11 +1,47 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import bg from "../../assets/halyna-stepanenko.jpg";
+import { useRef, useLayoutEffect } from "react";
+import gsap from "gsap";
+import { TextPlugin } from "gsap/TextPlugin"; // Import the TextPlugin
 
 const heroBg = {
   backgroundImage: `url(${bg})`, //ios background
 };
 
 export default function Hero() {
+  const text = useRef(null);
+  gsap.registerPlugin(TextPlugin); // Register the TextPlugin
+
+  useLayoutEffect(() => {
+    // create our context. This function is invoked immediately and all GSAP animations and ScrollTriggers
+    //created during the execution of this function get recorded so we can revert() them later (cleanup)
+    const ctx = gsap.context(() => {
+      //this will only select '.class' elements that are children of the component
+      gsap.from(".hi", {
+        xPercent: -450,
+        duration: 0.5,
+      });
+      gsap.from(".name", {
+        xPercent: -150,
+        duration: 0.5,
+        delay: 0.5,
+      });
+      gsap.to(".text", {
+        duration: 1.5,
+        delay: 1,
+        text: "Frontend Developer who transforms designs into elegant code that brings websites to life.",
+      });
+      gsap.from(".button", {
+        opacity: 0,
+        scale: 0.5,
+        duration: 0.5,
+        delay: 2.5,
+      });
+    }, text); // <- IMPORTANT! Scopes selector text
+
+    return () => ctx.revert(); // cleanup
+  }, []);
+
   const handleClick = () => {
     window.location.href = "mailto:galinavikst@gmail.com";
   };
@@ -29,6 +65,7 @@ export default function Hero() {
     >
       <Container sx={{ p: 0, display: "flex" }}>
         <Container
+          ref={text}
           sx={{
             alignSelf: "center",
             width: "65%",
@@ -36,21 +73,30 @@ export default function Hero() {
             m: 0,
           }}
         >
-          <Typography variant="h3" component={"p"} sx={{ fontSize: "2rem" }}>
+          <Typography
+            className="hi"
+            variant="h3"
+            component={"p"}
+            sx={{ fontSize: "2rem" }}
+          >
             Hi there, I am
           </Typography>
           <Typography
+            className="name"
             variant="h1"
             component={"h2"}
-            sx={{ fontFamily: "cursive, sans-serif", p: "0 0 10px" }}
+            sx={{ fontWeight: "bold", p: "0 0 10px" }}
           >
             Halyna Stepanenko
           </Typography>
-          <Typography variant="h3" component={"h1"}>
-            Frontend Developer who transforms designs into elegant code that
-            brings websites to life.
-          </Typography>
+          <Typography
+            className="text"
+            variant="h3"
+            component={"h1"}
+            sx={{ minHeight: 100 }}
+          ></Typography>
           <Button
+            className="button"
             variant="outlined"
             size="large"
             sx={{
